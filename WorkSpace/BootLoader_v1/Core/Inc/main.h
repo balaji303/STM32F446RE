d0dd79c38c48648cedfd7734cb6997356ad7aff4
Balaji <balajimail@zoho.com>
2022-09-17 07:08:35 +0530

    through Jamaica, a Buffalo Soldier

--- WorkSpace/BootLoader_v1/Core/Inc/main.h
+++ WorkSpace/BootLoader_v1/Core/Inc/main.h
@@ -72,7 +72,7 @@ void Error_Handler(void);
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
-
+#define BOOT_DEBUG_MSG		1
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus

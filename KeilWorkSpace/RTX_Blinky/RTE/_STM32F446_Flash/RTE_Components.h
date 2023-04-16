d836b6941d57dcd68f804fb215ecbcf8edd2bb71
Balaji <balajimail@zoho.com>
2023-04-16 07:41:34 +0530

    I wanna wrap my arms around you, baby, never let you go, oh

---
+++ KeilWorkSpace/RTX_Blinky/RTE/_STM32F446_Flash/RTE_Components.h
@@ -0,0 +1,26 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RTX_Blinky' 
+ * Target:  'STM32F446 Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+/*  ARM::CMSIS:RTOS:Keil RTX:4.82.0 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+/*  Keil::Device:Startup:2.6.3 */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+
+
+#endif /* RTE_COMPONENTS_H */
